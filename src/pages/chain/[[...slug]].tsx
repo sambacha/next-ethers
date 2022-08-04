6464611173b3d3477ce2c9d7db908735785f5c2a
sam bacha <sam@manifoldfinance.com>
2022-08-04 09:22:00 -0700

    Create [[...slug]].tsx

---
+++ src/pages/chain/[[...slug]].tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import dynamic from "next/dynamic";
+import { FileJs, Gear, Play } from "phosphor-react";
+import Hotkeys from "react-hot-keys";
+import Split from "react-split";
+import { useSnapshot } from "valtio";
+
+const Home: NextPage = () => {
+  const snap = useSnapshot(state);
+
+  return (
+
+// todo
+
+  );
+};
+
+export default Home;

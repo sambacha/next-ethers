a009065daa8404bcd141e38f62f36d102a977f3a
sam bacha <sam@manifoldfinance.com>
2022-08-04 09:22:15 -0700

    Update [[...slug]].tsx

--- src/pages/chain/[[...slug]].tsx
+++ src/pages/chain/[[...slug]].tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
-import { FileJs, Gear, Play } from "phosphor-react";
 import Hotkeys from "react-hot-keys";
 import Split from "react-split";
 import { useSnapshot } from "valtio";
